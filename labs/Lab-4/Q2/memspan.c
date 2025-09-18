06427ee4350ca8d0f03d5252b215de65a91240e2
LordSosukeAizen <gudipraneeth175@gmail.com>
2025-09-18 11:41:47 +0530

    change

--- labs/Lab-4/Q2/memspan.c
+++ labs/Lab-4/Q2/memspan.c
@@ -13,45 +13,45 @@ long how_large(void *address)
    *  TODO: Your code
    */ 
 
-unsigned long addr = (unsigned long) address;
-addr = (addr >> 12) << 12;
-void *ptr1 = mmap((void *)addr, _PB, PROT_READ | PROT_WRITE,  MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+        unsigned long addr = (unsigned long) address;
+        addr = (addr >> 12) << 12;
+        void *ptr1 = mmap((void *)addr, _PB, PROT_READ | PROT_WRITE,  MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
 
-assert(ptr1 != MAP_FAILED);
-munmap(ptr1, _PB);
+        assert(ptr1 != MAP_FAILED);
+        munmap(ptr1, _PB);
 
-if(ptr1 == (void *)addr)
-	return 0;
+        if(ptr1 == (void *)addr)
+                return 0;
 
-void *left = (void *) addr - 4096;
-void *right = (void *) addr + 4096;
+        void *left = (void *) addr - 4096;
+        void *right = (void *) addr + 4096;
 
 
-while(1){
-	void *allocated = mmap(left, _PB, PROT_READ | PROT_WRITE,  MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+        while(1){
+                void *allocated = mmap(left, _PB, PROT_READ | PROT_WRITE,  MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+                assert(allocated != MAP_FAILED);
+                munmap(allocated, _PB);
+                if(allocated == left){
+                        left += _PB;
+                        break;
+                }
+                left -= _PB;
+        }
+
+        while(1) {
+        void* allocated = mmap(right, _PB, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
         assert(allocated != MAP_FAILED);
         munmap(allocated, _PB);
-	if(allocated == left){
-		left += _PB;
-		break;
-	}
-	left -= _PB;
-}
+        if(allocated == right) break;
 
-while(1) {
-     void* allocated = mmap(right, _PB, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
-     assert(allocated != MAP_FAILED);
-     munmap(allocated, _PB);
-     if(allocated == right) break;
+        right += _PB;
 
-     right += _PB;
+        }
 
-}
+        long ans = right - left;
 
-long ans = right - left;
+        return ans;
 
-return ans;
-
-
+        
  
 }
